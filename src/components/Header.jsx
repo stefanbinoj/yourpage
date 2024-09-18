8611164eb5ad0ef2af77926f6928f1bc8adcbd61
stefan <stefan.binoj.007@gmail.com>
2024-09-18 19:31:26 +0530

    contactus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
@@ -33,12 +33,24 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={(isActive) =>
+                                to='/'
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 *:first-letter:
+                                        ${isActive?"text-orange-700":"text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to='/about'
+                                    className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 *:first-letter:
                                         ${isActive?"text-orange-700":"text-gray-500"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    Home
+                                    About 
                                 </NavLink>
                             </li>
                             
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/AboutUs.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App />,
+    children:[
+      {
+        path:'',
+        element:<Home />
+      },
+      {
+        path:'about',
+        element:<About />
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
