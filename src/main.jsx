dc3ae9bb79d55210049f927d73cf1920f8cd5ba4
stefan <stefan.binoj.007@gmail.com>
2024-09-19 11:24:37 +0530

    custom router

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import About from './components/AboutUs.jsx'
+import Contact from './components/ContactUs.jsx'
 
 const router=createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router=createBrowserRouter([
       {
         path:'about',
         element:<About />
+      },
+      {
+        path:'contact',
+        element:<Contact />
       }
     ]
   }
